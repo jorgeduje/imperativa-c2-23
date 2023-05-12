a7150e07b2bf803b648b6b5901683fa3e0bb91c2
jorgeduje <jorgeduje4@gmail.com>
2023-05-11 22:07:31 -0300

    .

--- variables.js
+++ variables.js
@@ -1,3 +1,71 @@
-let nombre = "Jorge"
+// let - const
 
-console.log(nombre)
+// variables
+
+let nombre = "pepito";
+
+console.log(nombre);
+
+nombre = "juan";
+
+nombre = 22;
+
+console.log(nombre);
+
+const APELLIDO = "Perez";
+
+// apellido = "Gonzalez"
+
+// TIPOS DE DATOS
+
+// string -- cadenada de texto
+let nombreDeUsuario = "jorge duje";
+
+// number 
+let edad = 30
+let precio = 15.5
+
+// booleanos  ---> true / false
+let esMayorDeEdad = true
+
+// undefined
+let numero = undefined
+
+let numero2 = null
+
+// NAN
+console.log( nombreDeUsuario * edad )
+
+console.log("---------------------------------------")
+console.log("---------------------------------------")
+console.log("---------------------------------------")
+// OPERADORES
+
+// matematicos
+
+let n1 = 10
+let n2 = 4
+
+console.log( n1 + n2 )
+console.log( n1 - n2 )
+console.log( n1 * n2 )
+console.log( n1 / n2 )
+
+console.log( n1 % n2  )
+
+let nombre1 = "gaston"
+let apellido1 = "lopez"
+
+// console.log("hola juancito perez como estas?")
+console.log( "Hola " + nombre1 + " " + apellido1 + " como estas?" )
+
+// template literals ---> backticks ``
+console.log( `Hola ${nombre1} ${apellido1} como estas? ${ 10 + 20 } ` )
+
+
+
+
+
+// logicos
+// comparacion 
+
