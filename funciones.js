1bc2688b96e50e046c91a61b5882b856670c17f7
jorgeduje <jorgeduje4@gmail.com>
2023-05-15 22:06:52 -0300

    se trabaja con funciones

---
+++ funciones.js
@@ -0,0 +1,68 @@
+
+
+// declardas vs expresadas ( 2 )
+
+
+// crear la funcion ---> declarar la funcion
+function sumar(){
+    // bloque de codigo
+    let n1 = 2
+    let n2 = 5
+    console.log("hola")
+    return n1 + n2
+}
+
+
+
+let resultado = sumar()
+console.log(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- variables.js
+++ variables.js
@@ -60,12 +60,89 @@ let apellido1 = "lopez"
 console.log( "Hola " + nombre1 + " " + apellido1 + " como estas?" )
 
 // template literals ---> backticks ``
-console.log( `Hola ${nombre1} ${apellido1} como estas? ${ 10 + 20 } ` )
+console.log( `Hola ${nombre1} ${apellido1} como estas? ${ 10 + 20 } `)
 
 
 
+// comparacion  ---> que siempre cuando se resuelve la comparacion
+//                   me devuelve un booleano
+
+console.log("-----------")
+let a = 2
+let b = 5
+
+let a_es_mayor = a > b 
+let b_es_mayor = a < b 
+console.log(a_es_mayor)
+console.log(b_es_mayor)
 
 
+console.log( a <= b)
+console.log( a >= b)
+
+console.log("----------")
+let c = 12
+let d = "12"
+// COMPARACION SIMPLE
+console.log( c == d )
+console.log( c != d )
+
+// COMPARACION ESTRICTA
+console.log( c === d)
+console.log( c !== d) // ---- c es distinto d
+
+console.log("Logicos")
 // logicos
-// comparacion 
+
+//negacion 
+
+let esMayor = true
+console.log(!esMayor) // --- lo contrario a "esMayor"
+
+console.log(esMayor)
+
+let distintoMayor = !esMayor
+
+
+
+console.log("-----------")
+console.log("-----------")
+console.log("-----------")
+console.log("-----------")
+console.log("-----------")
+console.log("-----------")
+
+// && --- || 
+
+// operador and ( && )
+// operador or ( || )
+
+let y = 15
+let z = 21
+let nombree = "pepito"
+
+// --> me devuelve el ultimo truthy o me devuelve el primer falsy
+let seCumple = null && 123123 && 0
+
+// el primer truthy --- o el ultimo falsy
+let seCumple2 = 0 || null || "" || undefined
+
+// console.log(seCumple)
+console.log(seCumple2)
+
+// truthy ---> true , "dasdasda", 1231, []
+// falsy ---> false , "", 0, null, undefined
+
+// logged === true
+// rol === "ADMIN"
+
+// Pantalla de modificar producto
+
+let isLogged = true
+let rol = "pepito"
+
+let tienePermiso = isLogged === true && rol === "ADMIN"
+
+console.log(tienePermiso ? "accede a la pantalla" : "no accede a la pantalla") 
+
 
